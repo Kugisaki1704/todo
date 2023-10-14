95a40608b88cf6185011ba35ba2bdce82259028b
Kugisaki1704 <sapnabora85@gmail.com>
2023-10-14 17:44:41 +0530

    loaded categories from databaset to screen

--- src/app/service/category.service.ts
+++ src/app/service/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { Action } from 'rxjs/internal/scheduler/Action';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,5 +14,18 @@ export class CategoryService {
     this.afs.collection('categories').add(data).then(ref => {
       this.toastr.success("New Category Saved Successfully");
     });
+  }
+
+  loadCategories()
+  {
+    return this.afs.collection('categories').observeSnapshotChanges().pipe(
+        map(actions => {
+            return actions.map(a:any => {
+              const data = a.payload.doc.data();
+              const id = a.payload.doc.id;
+              return {id,data};
+            })
+        })
+    );
   }
 }
