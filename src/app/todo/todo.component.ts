c47356fb973113c23a5806b4c1d78a308f00fb68
Kugisaki1704 <sapnabora85@gmail.com>
2023-10-16 18:06:45 +0530

    made a saveTodo method

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit } from '@angular/core';
+import { TodoService } from '../service/todo.service';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent{
+  catId : string='';
+  constructor(private todoService:TodoService, private activatedRoute : ActivatedRoute) {}
 
+  ngOnInit():void{
+    this.catId = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    console.log(this.catId);
+  }
+
+  onSubmit(f:NgForm){
+    let todo={
+      todo:f.value.todoText,
+      isCompleted : false
+    }
+    this.todoService.saveTodo(this.catId , todo);
+    f.resetForm();
+  }
 }
