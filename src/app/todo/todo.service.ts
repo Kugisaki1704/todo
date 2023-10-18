33cff38cd14a9b54a7a8939c6612031b033b4e3f
Kugisaki1704 <sapnabora85@gmail.com>
2023-10-18 22:36:26 +0530

    added loadTodos , updateTodo and deleteTodo

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore , AngularFirestoreCollection , AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { AngularFirestore , AngularFirestoreCollection , DocumentData} from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
 import { map } from 'rxjs/operators';
 import { Category } from "../service/category.model";
@@ -38,13 +38,38 @@ export class TodoService {
   
 loadTodos(id:string):Observable<Todo[]> {
   const categoryDocRef = this.afs.doc(`categories/${id}`);
-  // return categoryDocRef.collection('todos').valueChanges({ idField: 'id' }) as Observable<Todo[]>;
-    const todosCollection = categoryDocRef.collection('todos');
-    todosCollection.valueChanges({ idField: 'id' });
+  return categoryDocRef.collection('todos').valueChanges({ idField: 'id' }).pipe(
+    map((data: (DocumentData & { id: string; })[]) => {
+      // Map the data to the Todo type
+      return data.map(item => ({
+        id: item.id,
+        todo: item['todo'], // Assuming the 'todo' property is available in the DocumentData
+        // Map other properties of Todo as needed
+      }));
+    })
+  );
 }
+updateTodo(catId: string, todoId: string, updatedData: string): Promise<void> {
+  const todoDocRef = this.afs.doc(`categories/${catId}/todos/${todoId}`);
+  return todoDocRef.update({ todo: updatedData }).then(() => {
+    this.toastr.success('Todo Updated Successfully');
+  });
+  }
 
-  
+  deleteTodo(catId: string, todoId: string) {
+    const todoDocRef = this.afs.doc(`categories/${catId}/todos/${todoId}`);
+    return todoDocRef.delete().then(() => {
+      const categoryDocRef = this.afs.doc(`categories/${catId}`);
+      categoryDocRef.get().subscribe((doc: any) => {
+        const currentTodoCount = doc.data()?.todoCount || 0; // If todoCount doesn't exist, set it to 0
+        categoryDocRef.update({ todoCount: currentTodoCount > 0 ? currentTodoCount - 1 : 0 }); // Decrement the value or set to 0 if it's already 0
+        this.toastr.success('Todo Deleted Successfully');
+      }, (error: any) => {
+        this.toastr.error('Error getting document: ' + error);
+      });
+    }).catch((error: any) => {
+      this.toastr.error('Error deleting Todo: ' + error);
+    });
+  }
   }
   
-  
-
